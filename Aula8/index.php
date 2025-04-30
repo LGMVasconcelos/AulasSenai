5db0ad242ba2e87d972c72d356a376a343fc6d35
Luiz Gustavo Morais Vasconcelos <101607585+LuizGustavoMoraisVasconcelosPR@users.noreply.github.com>
2025-04-30 17:10:07 -0300

    Primeira aula sobre php!
    código de soma, subtração e média

---
+++ Aula8/index.php
@@ -0,0 +1,10 @@
+<?php
+    $n1 = 4;
+    $n2 = 5;
+    $soma = $n1 + $n2;
+    $subtracao = $n1 - $n2;
+    $media = ($n1 + $n2) /2;
+    echo "A soma de $n1 e $n2 é de: $soma <br>";
+    echo "A subtração de $n1 e $n2 é de: $subtracao <br>";
+    echo "A média de $n1 e $n2 é de: $media <br>";
+?>
