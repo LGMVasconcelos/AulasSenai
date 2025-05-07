a6f12a467efac4e987afe112058b4f69445f74d2
Luiz Gustavo Morais Vasconcelos <101607585+LuizGustavoMoraisVasconcelosPR@users.noreply.github.com>
2025-05-07 14:55:10 -0300

    Conversor de Celsius para Fahrenheit

---
+++ Aula11/Calculadora de Fahrenheit/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Conversor de temperatura</title>
+</head>
+
+<body>
+    <center>
+        <h2>CONVERSOR DE TEMPERATURA DE CELSIUS PARA FAHRENHEIT</h2>
+    </center>
+    <hr />
+    <div class="row justify-content-center row-cols-1 row-cols-md-2 mb-3 text-center">
+        <div class="col">
+            <div class="card mb-4 rounded-3 shadow-sw">
+                <div class="card-header py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-thermometer-half" viewBox="0 0 16 16">
+                        <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415" />
+                        <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1" />
+                    </svg>&nbsp;&nbsp;<font style="font-size: 30px;"><b>CONVERSOR DE TEMPERATURA</b></font>
+                </div>
+                <div class="card-body">
+                <form action="resultado.php" method="post">
+                                <label class="form-label">Digite abaixo uma temperatura em Celsius</label>
+                                <div class="input-group mb-3">
+                                    <input class="form-control" type="number" name="celsius" placeholder="Digite aqui uma temperatura em Celsius" step= "0.1" required>
+                                    <span class="input-group-text">C</span>
+                                </div>
+                        <br />
+                        <input type="submit" class="btn btn-outline-success" value="VERIFICAR" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
