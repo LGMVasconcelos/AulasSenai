b68b21e828c32445145fae2b2ee97a65aabcacf3
Luiz Gustavo Morais Vasconcelos <101607585+LuizGustavoMoraisVasconcelosPR@users.noreply.github.com>
2025-05-07 14:52:30 -0300

    Calculadora de médias de aluno

--- Aula11/Calculadora de médias/index.php
+++ Aula11/Calculadora de médias/index.php
@@ -7,7 +7,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Calculadora de notas</title>
 </head>
-
 <body>
     <center>
         <h2>CALCULADORA DE NOTAS</h2>
