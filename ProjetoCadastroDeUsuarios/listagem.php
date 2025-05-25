55a0b395cc63bc6c1494ddf51df005d59c65e312
Luiz Gustavo Morais Vasconcelos <101607585+LuizGustavoMoraisVasconcelosPR@users.noreply.github.com>
2025-05-25 00:49:44 -0300

    Página de listagem de usuários
    Página de listagem de usuários, na qual você pode localizar, excluir e editar dados de todos os usuários cadastrados com facilidade.

--- ProjetoCadastroDeUsuarios/listagem.php
+++ ProjetoCadastroDeUsuarios/listagem.php
@@ -140,7 +140,6 @@ $nomes = $_SESSION['nomes'];
         </div>
     </div>
 
-    <!-- Modal de Edição -->
     <div class='modal fade' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
         <div class='modal-dialog'>
             <div class='modal-content'>
